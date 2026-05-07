fc21485a786baa0bc3d041d105bdfee903eb1819
BMsStrength <bmsstrength@gmail.com>
2026-05-07 15:03:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE_NAME = 'trad-v1';
+var urlsToCache = ['/trad-reservation/', '/trad-reservation/index.html'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
